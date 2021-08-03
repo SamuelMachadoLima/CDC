2a5e4dd16675c0ad55c8818786e3f9801734a452
SamuelMachadoLima <devsamuelm@gmail.com>
2021-08-03 09:55:49 -0300

    Teste Heroku

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var app = require("./config/server");
-
+var http = require("http");
 // app.listen(process.env.PORT, function(){
 //     console.log("Servidor ON");
 // });
 
-app.createServer(function (req, res){
+http.createServer(function (req, res){
     res.writeHead(200, {"Content-Type": "text/plain"})
     res.end("Hello Wolrd\n")
 }).listen(process.env.PORT);
